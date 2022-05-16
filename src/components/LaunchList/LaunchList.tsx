b5ffa5c670baf007e183ed985b8f86f993ed3ad9
Steph Mason <stephanie.mason@99designs.com>
2022-05-16 14:44:49 +1000

    fix linting error

--- src/components/LaunchList/LaunchList.tsx
+++ src/components/LaunchList/LaunchList.tsx
@@ -19,11 +19,12 @@ const LaunchList: React.FC<Props> = ({ data, handleIdChange }) => (
       {!!data.launches &&
         data.launches.map(
           (launch, i) =>
-            !!launch && (
+            !!launch &&
+            launch.flight_number && (
               <li
                 key={i}
                 className={`${className}__item`}
-                onClick={() => handleIdChange(launch.flight_number!)}
+                onClick={() => handleIdChange(launch.flight_number!)} // eslint-disable-line
               >
                 {launch.mission_name} ({launch.launch_year})
               </li>
