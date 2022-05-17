dea95758de75c512a3bcbc3b844cc1ad5e0e9bec
Steph Mason <stephanie.mason@99designs.com>
2022-05-17 11:26:11 +1000

    fix lint issue

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { MockedProvider } from '@apollo/client/testing';
+import { MockedProvider } from '@apollo/client/testing'
 import App from './App'
 
-
-
 test('renders SpaceApp heading', () => {
-  const component = <MockedProvider><App /></MockedProvider>
+  const component = (
+    <MockedProvider>
+      <App />
+    </MockedProvider>
+  )
   render(component)
-  const linkElement = screen.getByText("SpaceApp")
+  const linkElement = screen.getByText('SpaceApp')
   expect(linkElement).toBeInTheDocument()
 })
