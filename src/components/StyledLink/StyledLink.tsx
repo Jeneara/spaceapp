c93b71da317d4ec12b178893a3cba68cc32b2efb
Steph Mason <50791356+Jeneara@users.noreply.github.com>
2022-05-30 11:21:01 +1000

    Add styled-components and babel (#6)
    * Add styled-components and babel
    
    * prettier

---
+++ src/components/StyledLink/StyledLink.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  background: white;
+  padding: 0 10px 2px;
+  width: auto;
+  color: palevioletred;
+  border: 2px solid;
+  border-color: palevioletred;
+  border-radius: 20px;
+  font-weight: bold;
+  margin-top: 5px;
+  font-size: 20px;
+`
+
+export default StyledLink
